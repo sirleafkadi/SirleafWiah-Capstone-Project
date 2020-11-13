b431eb8398014840271432c2224875aab1398e48
sirleafb9 <sirleafb9@gmail.com>
2020-11-13 08:21:29 -0600

    Inital

---
+++ src/app/app-routing-module.ts
@@ -0,0 +1,12 @@
+import {Routes} from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+
+export const applicationRoutes: Routes =[
+        {path:'', redirectTo:'home', pathMatch:'full'},
+        {path:'home', component:HomeComponent}
+
+
+
+
+]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import{FormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
+import {applicationRoutes} from './app-routing-module'
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(applicationRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
